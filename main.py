48f57b351e6128e69de28473aa9a10161ab02b16
Ali Dorostkar <alidorostkar.dev@gmail.com>
2026-05-03 23:42:52 +0200

    feat(api): fetch bitcoin and ethereum prices from coingecko

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+import requests
+
+def get_prices():
+    crypto_data = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd").json()
+    btc = crypto_data["bitcoin"]["usd"]
+    eth = crypto_data["ethereum"]["usd"]
+
+    return btc, eth
+
 def main():
-    pass
+    print(get_prices())
 
 if __name__ == "__main__":
     main()
