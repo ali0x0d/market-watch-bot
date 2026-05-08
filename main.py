310946f5c56721348ff7f9bad9d607192074110c
Ali Dorostkar <alidorostkar.dev@gmail.com>
2026-05-09 00:45:14 +0200

    feat(prices): add gold, silver, and oil via yfinance

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import requests
+import yfinance as yf
 
 def get_prices():
     prices = {}
 
+    # btc, eth
     crypto_data = requests.get("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin,ethereum&vs_currencies=usd").json()
     prices["btc"] = crypto_data["bitcoin"]["usd"]
     prices["eth"] = crypto_data["ethereum"]["usd"]
+
+    # gold, silver, oil
+    symbols = ["GC=F", "SI=F", "CL=F"]
+    assets = yf.Tickers(symbols)
+    prices["gold"] = assets.tickers["GC=F"].fast_info['last_price']
+    prices["silver"] = assets.tickers["SI=F"].fast_info['last_price']
+    prices["oil"] = assets.tickers["CL=F"].fast_info['last_price']
 
     return prices
 
